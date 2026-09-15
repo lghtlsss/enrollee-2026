94a04abbae50ee51b960800f103a3e637b14fa52
gamerus12125 <miska3040@gmail.com>
2026-09-15 23:41:50 +0300

    добавил формы регистрации и входа

---
+++ frontend/src/components/auth/register-form.tsx
@@ -0,0 +1,65 @@
+'use client';
+import { REGISTER_URL } from '@/utils/constants';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { SubmitEvent, useState } from 'react';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+
+const register = async ({ email, password }: { email: string; password: string }) => {
+  const response = await fetch(REGISTER_URL, {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+  });
+  return response.json();
+};
+
+export const RegisterForm = () => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const router = useRouter();
+  const { isPending, mutate } = useMutation({
+    mutationFn: register,
+    onSuccess: () => router.push('#'),
+    onError: e => setErrorMessage(e.message),
+  });
+
+  const handleSubmit = (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+    const passwordRepeat = formData.get('password-repeat')?.toString();
+    if (email && password && password == passwordRepeat) {
+      mutate({ email, password });
+    } else {
+      setErrorMessage('Пароли должны совпадать');
+    }
+  };
+
+  return (
+    <form className="flex w-125 flex-col gap-2" onSubmit={handleSubmit}>
+      <Input id="email" name="email" type="email" placeholder="ваша@почта.com" required={true} />
+      <Input
+        className="border-2 p-2"
+        id="password"
+        name="password"
+        type="password"
+        placeholder="пароль"
+        onChange={_ => setErrorMessage(null)}
+        required={true}
+      />
+      <Input
+        className="border-2 p-2"
+        id="password-repeat"
+        name="password-repeat"
+        placeholder="повторите пароль"
+        onChange={_ => setErrorMessage(null)}
+        required={true}
+      />
+      <Button disabled={isPending} type="submit">
+        Зарегестрироваться
+      </Button>
+      {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+    </form>
+  );
+};
---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const LOGIN_URL = 'https://login.ru';
+export const REGISTER_URL = 'https://register.ru';
