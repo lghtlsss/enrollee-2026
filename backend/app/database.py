7e164a9750f019326c5c9243632612603a4da9e6
1lght <guryanovartem2008@gmail.com>
2026-09-15 21:04:01 +0300

    добавил загрузку юрл бд и секретов jwt из .env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,17 @@
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Base(DeclarativeBase):
     pass
 
 
-DB_URL = ""  # сделать через .env
-
+DB_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
