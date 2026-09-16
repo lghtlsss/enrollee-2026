38a0effe861ad8576713bda28288eb451f7295f9
1lght <guryanovartem2008@gmail.com>
2026-09-16 23:09:52 +0300

    Поправил чуть названия импортов роутеров

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI
 
 from app.schemas import SInputPoints
 
-from app.routers import universities, directions, programs, recommendations, user_router, auth_router
+from app.routers import universities_router, directions_router, programs_router, recommendations_router, user_router, \
+    auth_router
+
 app = FastAPI(title="Enrollee-2026")
 
 app.include_router(auth_router)
 app.include_router(user_router)
-app.include_router(universities.router)
-app.include_router(directions.router)
-app.include_router(programs.router)
-app.include_router(recommendations.router)
+app.include_router(universities_router)
+app.include_router(directions_router)
+app.include_router(programs_router)
+app.include_router(recommendations_router)
 
 
 @app.get("/")
--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,2 +1,6 @@
 from .auth import router as auth_router
 from .user import router as user_router
+from .directions import router as directions_router
+from .universities import router as universities_router
+from .programs import router as programs_router
+from .recommendations import router as recommendations_router
