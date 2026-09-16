818b73e87a9dfcdc05db4f245b55ef83d28d9c0d
gamerus12125 <miska3040@gmail.com>
2026-09-16 18:39:04 +0300

    добавил тип для объекта университета

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,11 @@
+export type University = {
+  id: number;
+  name: string;
+  city: string;
+  description: string;
+  passingScore: number;
+  freePlacesCount: number;
+  educationCost: number;
+  rating: number;
+  vibeValue: number;
+};
