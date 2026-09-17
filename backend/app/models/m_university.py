052312b73dd55c8c3f952749425191eae65201af
1lght <guryanovartem2008@gmail.com>
2026-09-17 22:31:50 +0300

    Кривой отступ

--- backend/app/models/m_university.py
+++ backend/app/models/m_university.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
+
 class University(Base):
     """Вузы"""
     __tablename__ = "universities"
@@ -28,7 +29,7 @@ class University(Base):
     vibe: Mapped["UniversityVibe | None"] = relationship(
         back_populates="university", uselist=False, cascade="all, delete-orphan"
     )
-      reviews: Mapped[list["Review"]] = relationship(
+    reviews: Mapped[list["Review"]] = relationship(
         "Review",
         back_populates="university", cascade="all, delete-orphan"
     )
